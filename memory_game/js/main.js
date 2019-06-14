3b08572001117e983399c6129e220cc474013146
Dennis <trandennis0@gmail.com>
2019-06-13 23:56:44 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("UP and running!");
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+console.log("user flipped"+ " " + cardThree + " and " + cardTwo);
