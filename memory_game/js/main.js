b1829a1ead97232fc36e0e533a878e338d7615ad
Dennis <trandennis0@gmail.com>
2019-06-14 13:30:39 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,7 +2,28 @@ console.log("UP and running!");
 
 //console.log("user flipped"+ " " + cardThree + " and " + cardTwo);
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  },
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -24,8 +45,10 @@ function flipCard(cardId) {
       alert("Sorry, try again");
     }
   }
-  cardsInPlay.push(cards[cardId]);
-  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId].suit);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 }
 
 flipCard(0);
